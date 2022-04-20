5a985ce4958b306cda6bf9c0f41ddcd9b1dd088a
hachiyuki8 <caladuil@outlook.com>
2022-04-20 13:30:11 -0400

    bruh7

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,6 +4,6 @@ class Article < ApplicationRecord
   validates_presence_of :title 
   validates_presence_of :content
 
-  scope :active, 			 -> { where('active = ?', true) }
+  scope :active, -> { where('active = ?', true) }
   scope :alphabetical, -> { order('title') }
 end
